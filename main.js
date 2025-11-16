ecfcb60a22595551b47e3d0525a9b50458915f2b
BaharYazdanshenas <186168285+BaharYazdanshenas@users.noreply.github.com>
2025-11-16 20:05:45 +0100

    clean version ready to start the assignment 2

--- main.js
+++ main.js
@@ -5,19 +5,6 @@ window.onload = () => {
   console.log("YOUR CODE GOES HERE");
 };
 
-// Load the data set from the assets folder:
-  console.log("D3 assignment test");
-
-const svg = d3.select("body")
-  .append("svg")
-  .attr("width", 400)
-  .attr("height", 300);
-
-svg.append("circle")
-  .attr("cx", 100)
-  .attr("cy", 150)
-  .attr("r", 30)
-  .attr("fill", "steelblue");
 
 d3.csv("cars.csv").then(function(data) {
     console.log("Cars data:", data);
