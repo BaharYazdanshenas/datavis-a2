6bb04e995accc642132b7188fc908e1744490599
BaharYazdanshenas <bahar.yazdanshenas@ph.tum.de>
2025-11-16 14:01:11 +0100

    Add test D3 circle

--- main.js
+++ main.js
@@ -5,5 +5,16 @@ window.onload = () => {
   console.log("YOUR CODE GOES HERE");
 
   // Load the data set from the assets folder:
+  console.log("D3 assignment test");
 
+const svg = d3.select("body")
+  .append("svg")
+  .attr("width", 400)
+  .attr("height", 300);
+
+svg.append("circle")
+  .attr("cx", 100)
+  .attr("cy", 150)
+  .attr("r", 30)
+  .attr("fill", "steelblue");
 };
