3cdf822f9a7be4effacbf1a5ab2e8cdb3f22c0ac
BaharYazdanshenas <186168285+BaharYazdanshenas@users.noreply.github.com>
2025-11-18 22:54:31 +0100

    Trial Scatter Plot - Width vs. Length + AWD

--- main.js
+++ main.js
@@ -8,5 +8,94 @@ window.onload = () => {
 
 d3.csv("cars.csv").then(function(data) {
     console.log("Cars data:", data);
-    window.carsData = data;  // <- add this line just for exploration
+    window.carsData = data;
+
+    const svgNode = chart(data);      // <- get what chart() returns
+    document.body.appendChild(svgNode); // <- attach it to the page
 });
+
+function chart(data) {
+
+  // Specify the chart’s dimensions.
+  const width = 928;
+  const height = 600;
+  const marginTop = 25;
+  const marginRight = 20;
+  const marginBottom = 35;
+  const marginLeft = 40;
+
+  // Create the positional scales.
+  const x = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.Len)).nice()
+    .range([marginLeft, width - marginRight]);
+  const y = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.Width)).nice()
+    .range([height - marginBottom, marginTop]);
+
+    // Create the categorical scales.
+  const color = d3.scaleOrdinal(data.map(d => d.AWD), d3.schemeCategory10);
+  const shape = d3.scaleOrdinal(data.map(d => d.AWD), d3.symbols.map(s => d3.symbol().type(s)()));
+
+  // Create the SVG container.
+  const svg = d3.create("svg")
+      .attr("viewBox", [0, 0, width, height])
+      .attr("width", width)
+      .attr("height", height)
+      .attr("style", "max-width: 100%; height: auto;");
+
+      // Append the axes.
+  svg.append("g")
+      .attr("transform", `translate(0,${height - marginBottom})`)
+      .call(d3.axisBottom(x).ticks(width / 80))
+      .call(g => g.select(".domain").remove())
+      .call(g => g.append("text")
+          .attr("x", width)
+          .attr("y", marginBottom - 4)
+          .attr("fill", "currentColor")
+          .attr("text-anchor", "end")
+          .text("Car length (cm) →"));
+
+  svg.append("g")
+      .attr("transform", `translate(${marginLeft},0)`)
+      .call(d3.axisLeft(y))
+      .call(g => g.select(".domain").remove())
+      .call(g => g.append("text")
+          .attr("x", -marginLeft)
+          .attr("y", 10)
+          .attr("fill", "currentColor")
+          .attr("text-anchor", "start")
+          .text("↑ Car width (cm)"));
+
+// Add a grid.
+  svg.append("g")
+      .attr("stroke", "currentColor")
+      .attr("stroke-opacity", 0.1)
+      .call(g => g.append("g")
+        .selectAll("line")
+        .data(x.ticks())
+        .join("line")
+          .attr("x1", d => 0.5 + x(d))
+          .attr("x2", d => 0.5 + x(d))
+          .attr("y1", marginTop)
+          .attr("y2", height - marginBottom))
+      .call(g => g.append("g")
+        .selectAll("line")
+        .data(y.ticks())
+        .join("line")
+          .attr("y1", d => 0.5 + y(d))
+          .attr("y2", d => 0.5 + y(d))
+          .attr("x1", marginLeft)
+          .attr("x2", width - marginRight));
+// Add the scatterplot symbols.
+  svg.append("g")
+      .attr("stroke-width", 1.5)
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 10)
+    .selectAll("path")
+    .data(data)
+    .join("path")
+      .attr("transform", d => `translate(${x(d.Len)},${y(d.Width)})`)
+      .attr("fill", d => color(d.AWD))
+      .attr("d", d => shape(d.AWD));
+  return svg.node();
+}
